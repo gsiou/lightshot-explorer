e548d5bd44f8b749fe742d70b1324f8510459222
Georgios Anastasiou <dr.george995@gmail.com>
2018-03-09 00:28:45 +0200

    Basic frontend app

---
+++ web/src/components/Main.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1 v-if="loading">Loading...</h1>
+    <img :src="image"/>
+    <br/>
+    <button v-on:click="getPrev">Prev</button>
+    <button v-on:click="getNext">Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    img: String
+  },
+  data: function () {
+    return {
+      image: '',
+      next: '',
+      prev: '',
+      loading: false
+    }
+  },
+  mounted () {
+    this.getImage(this.img)
+  },
+  methods: {
+    getImage(image) {
+      this.loading = true
+      window.fetch('http://localhost:12345/image/' + image)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        this.image = res.image
+        this.next = res.next
+        this.prev = res.prev
+        this.loading = false
+      })  
+    },
+    getNext() {
+      this.getImage(this.next)
+    },
+    getPrev() {
+      this.getImage(this.prev)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
